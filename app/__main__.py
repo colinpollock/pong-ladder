48e7f13950107de30c6b3dfc1d9bc916085c2784
Colin <pollockcolin@gmail.com>
2015-12-12 20:24:34 -0800

    Pulling the app creation function out of the package runner __main__.py script so it can be called from elsewhere.

--- app/__main__.py
+++ app/__main__.py
@@ -4,36 +4,6 @@ The app will be configured to run in a particular environment, specified by the
 `FLASK_ENV` environment variable.
 """
 
-import os
-
-from flask.ext.restful import Api
-from flask import Flask
-from flask_environments import Environments
-
-from models import db
-from resource import (PlayerListResource, PlayerResource, GameListResource,
-                      ChallengeListResource)
-
-
-def create_app():
-    """Return a Flask app configured for the correct env (test, dev, prod)."""
-    app = Flask(__name__)
-    env = Environments(app)
-    env.from_yaml(os.path.join(os.getcwd(), 'config.yaml'))
-
-    api = Api(app)
-    api.add_resource(PlayerListResource, '/players')
-    api.add_resource(PlayerResource, '/players/<string:name>')
-    api.add_resource(GameListResource, '/games')
-    api.add_resource(ChallengeListResource, '/challenges')
-
-    db.init_app(app)
-
-    return app
-
-
-
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(port=app.config['PORT'], debug=app.config['DEBUG'])
+from app import create_app
+app = create_app()
+app.run(port=app.config['PORT'], debug=app.config['DEBUG'])
